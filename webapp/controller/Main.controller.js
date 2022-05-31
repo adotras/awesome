2000108b6888f35ebaa06bff143c33460c36c43d
RRichteradesso <robert.richter@adesso-orange.com>
2022-05-31 14:47:00 +0200

    odata

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,34 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/Sorter"
+], function(Controller, Filter, FilterOperator, Sorter) {
 	"use strict";
 
 	return Controller.extend("aweawesome.controller.Main", {
+		onInit: function() {
+			var oModel = this.getOwnerComponent().getModel();
+
+			this.oMyODataModel = oModel;
+
+			oModel.read("/Products", {
+
+				success: function(oData, response) {
+					console.log(oData);
+				},
+				error: function(oError) {
+					console.log("Ein Fehler ist aufgetreten!")
+					console.log(oError);
+				}
+			});
+		},
+		onPress: function() {
+			var oSomeCategory = this.oMyODataModel.getProperty("/Products");
+			console.log("Ergebnis von getProperty()");
+			console.log(oSomeCategory)
+
+		}
 
 	});
 });
