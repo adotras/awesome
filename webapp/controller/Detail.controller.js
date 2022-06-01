4cf12da1d258de47b5ec9db14886534a6f83772f
RRichteradesso <robert.richter@adesso-orange.com>
2022-06-01 18:40:10 +0200

    puschen

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/Fragment"
-], function(Controller, Fragment) {
+	"sap/ui/core/Fragment",
+	"sap/ui/core/routing/History"
+], function(Controller, Fragment, History) {
 	"use strict";
 
 	return Controller.extend("aweawesome.controller.Detail", {
@@ -16,9 +17,74 @@ sap.ui.define([
 			var sProdPath = oEvent.getParameter("arguments").prodId;
 
 			this.getView().bindElement({
-				path: "/Products/" + sProdPath,
-				model: "Default"
+				//				path: "/Products/" + sProdPath,
+				path: "/" + sProdPath
+					//				model: "undefined"
 			});
+		},
+		onNavBack: function() {
+			var oHistory = History.getInstance();
+			var oPrevHash = oHistory.getPreviousHash();
+
+			if (oPrevHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.toMaster();
+			}
+		},
+		onLiveChangeInputValidate: function(oEvent) {
+			var inputValue = oEvent.getParameter('value').trim();
+			var newValue = inputValue;
+
+			//determine the value range that should be allowed
+			var sCondition = oEvent.getSource().data()["liveChangeAllow"];
+			sCondition = sCondition ? sCondition : "";
+
+			newValue = newValue.replace(/[^\d]/g, "");
+
+			//determine max length to be allowed
+			var maxLength = oEvent.getSource().data()["liveChangeMaxLength"];
+			if (maxLength) {
+				newValue = newValue.substring(0, maxLength);
+			}
+			
+			var msgOnInvalidInput = oEvent.getSource().data()["liveChangeMsgOnInvalidInput"];
+			if (msgOnInvalidInput && (newValue !== inputValue)) {
+				for (var i = 0; i < inputValue.length; i++) {
+					if (newValue[i] !== inputValue[i]) {
+						msgOnInvalidInput = msgOnInvalidInput.replace('#', inputValue[i]);
+						if (i === maxLength) {
+							msgOnInvalidInput = "Exceeds max length:" + maxLength;
+						}
+						//alert(msgOnInvalidInput);
+						oEvent.getSource().setValue(newValue);
+						oEvent.getSource().setValueState("Warning");
+						oEvent.getSource().setValueStateText(msgOnInvalidInput);
+						break;
+					}
+				}
+			} else {
+				
+				
+				var myVal = event.target.value;
+				console.log(myVal);
+				
+				
+				var coolput = this.getView().byId("input2");
+				console.log(coolput);
+				coolput.setText(myVal);
+				// var input = document.getElementById("input2");
+				// console.log(input);
+				
+				//	this.getView().byId("input2").setText(this.getView().byId("input1").getValue());
+				// this.getView().byId("input2").valueOf(myVal);
+				// this.getView().byId("input2").value = myVal;
+				// this.getView().byId("input2").setText(myVal);
+				
+				oEvent.getSource().setValue(newValue);
+				oEvent.getSource().setValueState("None");
+				oEvent.getSource().setValueStateText("");
+			}
 		}
 	});
 });
